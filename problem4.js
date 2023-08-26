35cbb06868750ca86af9629a81af11fae21a64c8
Shivandru <shivandru.singh045@gmail.com>
2023-08-26 10:43:05 +0530

    Introduction to JS(variables) problems

---
+++ problem4.js
@@ -0,0 +1,29 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// var name = "Shivandru";
+// var school = "St.Xaviers's School";
+// var grade = 12;
+// var section = "A";
+// var roll_Number = 41;
+// var subject1 = 90;
+// var subject2=80;
+// var subject3=85;
+// console.log(name, school, grade, section, roll_Number);
+// console.log(subject1, subject2, subject3);
+var a= "Name: Shivandru";
+var b= "School: St.Xavier's School";
+var c= "Grade: 12";
+var d= "Section: A";
+var e= "subject1: marks= 80";
+var f= "subject2: marks=90";
+var g= "subject3: marks=100";
+console.log("████████████████████████████████████████");
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log("*****************************************");
+console.log(e);
+console.log(f);
+console.log(g);
+console.log("████████████████████████████████████████");
